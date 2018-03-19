bc5249b6a79a0ee79b469594c639a8714cb80e92
Mila Zangirolame <milazangirolame@gmail.com>
2018-03-19 19:21:23 -0300

    fixing controller name

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :html } do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :organizations, only: [ :index ]
     end
